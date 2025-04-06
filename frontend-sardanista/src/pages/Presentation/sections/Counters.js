bfa66d18248ab1c41b18ae9ad51752566ca79f50
montserramia <montserramia68@gmail.com>
2025-04-06 15:41:50 +0200

    Canvis en colors i navbar inicials

---
+++ frontend-sardanista/src/pages/Presentation/sections/Counters.js
@@ -0,0 +1,65 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={900}
+              suffix="+"
+              title="Socis Actius"
+              description="L’Agrupació compta amb una base sòlida de membres compromesos que mantenen viva la sardana."
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={33}
+              title="Anys d'història"
+              description="Tres dècades de dansa, cultura i tradició al cor de Castelldefels, oberts a totes les generacions.
+
+"
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={45}
+              suffix="+"
+              title="Aplecs i trobades"
+              description="Organitzem i participem en esdeveniments arreu del país, consolidant una presència activa i festiva."
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
